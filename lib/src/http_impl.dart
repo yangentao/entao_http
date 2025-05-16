9358aad554d88bf852c6eff8367c41378e6b0032
Entao <entaoyang@163.com>
2025-05-17 01:21:55 +0800

    1.0.1

--- lib/src/http_impl.dart
+++ lib/src/http_impl.dart
@@ -264,7 +264,12 @@ class HttpResult {
     null => httpOK ? (isJson ? jsonResult.msg : "OK") : switch (httpCode) { 401 => "401 未认证", 403 => "403 没有权限", 404 => "404 客户端错误", _ => "$httpCode $httpStatus" },
     _ => error.toString()
   };
-
+  ListResult<T> table<T>(T Function(JsonValue e) maper) {
+    if (success) {
+      return ListResult<T>.success(jsonResult.tableData(maper), code: code, message: message, rawResult: this, offset: jsonResult.offset, total: jsonResult.total);
+    }
+    return ListResult<T>.failed(code: code, message: message, error: error, rawResult: this);
+  }
   ListResult<T> list<T>(T Function(JsonValue e) maper) {
     if (success) {
       return ListResult<T>.success(jsonResult.listData(maper), code: code, message: message, rawResult: this, offset: jsonResult.offset, total: jsonResult.total);
