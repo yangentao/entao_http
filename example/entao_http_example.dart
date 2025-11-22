6cbacce53f9bea469af761329407c7a712d31512
Entao <entaoyang@163.com>
2025-11-22 10:48:52 +0800

    refact

--- example/entao_http_example.dart
+++ example/entao_http_example.dart
@@ -1,6 +1,11 @@
+import 'package:entao_dutil/entao_dutil.dart';
+import 'package:entao_http/entao_http.dart';
+import 'package:println/println.dart';
 
-
-void main() {
-  // var awesome = Awesome();
-  // print('awesome: ${awesome.isAwesome}');
+void main() async {
+  HttpResult hr = await httpGet(Uri.parse("http://localhost:8080/hole/pub/echo"), args: ["name" >> "entao", "age" >> 44], headers: {"locale": "zh_CN"});
+  println(hr.code, hr.message);
+  // 200 OK
+  println(hr.bodyText);
+  // {"age":"44","name":"entao","headers":{"accept-encoding":"gzip","host":"localhost:8080","locale":"zh_CN","user-agent":"Dart\/3.8 (dart:io)"}}
 }
